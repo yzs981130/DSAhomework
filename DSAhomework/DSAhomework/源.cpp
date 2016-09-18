6d910246446d8f50ee189f32d8160a753f2b38ed
叶志晟 <yzs981130@126.com>
2016-09-18 18:07:03 +0800

    多项式加法STL

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,82 +1,29 @@
-#include <iostream> 
+#include <iostream>  
+#include <map>  
 using namespace std;
-struct item {
-	int k, ex;
-	item *next;
-};
-item equalmark;
 
-item *search(item *current, item *head)
-{
-	while (head->next != NULL)
-	{
-		if (current->ex == head->next->ex)
-		{
-			head->next->k += current->k;
-			delete current;
-			return &equalmark;
-		}
-		if (current->ex > head->next->ex)
-			break;
-		head = head->next;
-	}
-	return head;
-}
-
-int main()
-{
-	int inputnum;
-	cin >> inputnum;
-	while (inputnum--)
-	{
-		item *head = NULL, *current = NULL;
-		int k, ex;
-		for (int i = 0; i <= 1; i++)
-		{
-			while (cin >> k >> ex, ex >= 0)
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i != n; ++i) {
+		map <int, int> p;
+		int ex, value, cnt = 0;
+		while (cin >> value >> ex) {
+			if (ex < 0)
+				cnt++;
+			else 
 			{
-				current = new item;
-				current->k = k, current->ex = ex, current->next = NULL;
-				if (head == NULL)
-					head = current;
-				else if (current->ex > head->ex)
-				{
-					current->next = head;
-					head = current;
-				}
-				else if (current->ex == head->ex)
-				{
-					head->k += current->k;
-					delete current;
-				}
-				else
-				{
-					item *temp = search(current, head);
-					if (temp != &equalmark)
-					{
-						current->next = temp->next;
-						temp->next = current;
-					}
-				}
+				pair <map <int, int>::iterator, bool> ret = p.insert(make_pair(ex, value));
+				if (!ret.second)
+					ret.first->second += value;
 			}
+			if (cnt == 2)
+				break;
 		}
-		current = head;
-		while (current->next != NULL)
-		{
-			if (current->k != 0)
-				cout << "[ " << current->k << ' ' << current->ex << " ] ";
-			current = current->next;
-		}
-		if (current->k != 0)
-			cout << "[ " << current->k << ' ' << current->ex << " ] "; 
-		if (inputnum > 0)
-			cout << endl;
-		while (head != NULL)
-		{
-			item *temp = head->next;
-			delete head;
-			head = temp;
-		}
+		for (map <int, int>::reverse_iterator riter = p.rbegin(); riter != p.rend(); ++riter)
+			if (riter->second != 0)
+				cout << "[ " << riter->second << ' ' << riter->first << " ] ";
+		cout << endl;
 	}
 	system("pause");
 	return 0;
