314df93302b1e57896529498349b0fe64b3276b9
Tizen Ye <yzs981130@126.com>
2016-10-28 15:00:25 +0800

    拼写检查

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,130 +1,134 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <string>
-#include <sstream>
+#include <iostream> 
+#include <stdio.h> 
+#include <string.h> 
+#include <algorithm> 
+#include <stdlib.h> 
+#include <string> 
 using namespace std;
-int t = 0, cnt = 0;
-enum TOKEN_TYPE
+
+#define max1 10005 
+#define max2 16 
+void find(char dic[][16], char s[], int n);
+//用来保存某个单词所有相似单词的数据结构(used to store all replacements of a query) 
+struct box
 {
-	TYPE_NUMBER,
-	TYPE_UNIT,
-	TYPE_NEGATIVE
+	int num[10005]; //记录相似单词在词典中的位置(record the position of all replacements) 
+	int top; //相似单词总数(the amount of the replacements) 
 };
-struct Token
+int main()
 {
-	string        StringValue;
-	int            Value;
-	TOKEN_TYPE    Type;
-};
-Token sTokenTable[] =
-{
-	{ "zero",        0, TYPE_NUMBER },
-	{ "one",            1, TYPE_NUMBER },
-	{ "two",            2, TYPE_NUMBER },
-	{ "three",        3, TYPE_NUMBER },
-	{ "four",        4, TYPE_NUMBER },
-	{ "five",        5, TYPE_NUMBER },
-	{ "six",            6, TYPE_NUMBER },
-	{ "seven",        7, TYPE_NUMBER },
-	{ "eight",        8, TYPE_NUMBER },
-	{ "nine",        9, TYPE_NUMBER },
-	{ "ten",            10, TYPE_NUMBER },
-	{ "eleven",        11, TYPE_NUMBER },
-	{ "twelve",        12, TYPE_NUMBER },
-	{ "thirteen",    13, TYPE_NUMBER },
-	{ "fourteen",    14, TYPE_NUMBER },
-	{ "fifteen",        15, TYPE_NUMBER },
-	{ "sixteen",        16, TYPE_NUMBER },
-	{ "seventeen",    17, TYPE_NUMBER },
-	{ "eighteen",    18, TYPE_NUMBER },
-	{ "nineteen",    19, TYPE_NUMBER },
-	{ "twenty",        20, TYPE_NUMBER },
-	{ "thirty",        30, TYPE_NUMBER },
-	{ "forty",        40, TYPE_NUMBER },
-	{ "fifty",        50, TYPE_NUMBER },
-	{ "sixty",        60, TYPE_NUMBER },
-	{ "seventy",        70, TYPE_NUMBER },
-	{ "eighty",        80, TYPE_NUMBER },
-	{ "ninety",        90, TYPE_NUMBER },
-	{ "hundred",        100, TYPE_UNIT },
-	{ "thousand",    1000, TYPE_UNIT },
-	{ "million",        1000000, TYPE_UNIT },
-	{ "negative",    -1, TYPE_NEGATIVE }
-};
-string GetTokenStr(const string &value)
-{
-	cnt = 0;
-	stringstream ss;
-	string str[30];
-	ss << value;
-	while (ss)
+	char diction[max1][max2];
+	int dict_num;
+	char s[16];
+	//读入并存储词典(store the dictionary) 
+	cin >> s;
+	dict_num = -1;
+	while (s[0] != '#')
 	{
-		ss >> str[cnt];
-		cnt++;
+		dict_num++;
+		strcpy(diction[dict_num], s);
+		cin >> s;
 	}
-	if (!t)
-		return str[cnt - 2];
-	return str[cnt - t - 1];
-}
-bool GetToken(const string &value, Token &nextToken)
-{
-	bool bRet = false;
-	string tokenStr = GetTokenStr(value);
-	t++;
-
-	for (int i = 0; i < 32; ++i)
+	//在词典中查询单词(search for the word in the dictionary) 
+	cin >> s;
+	while (s[0] != '#')
 	{
-		if (tokenStr == sTokenTable[i].StringValue)
-		{
-			nextToken.Type = sTokenTable[i].Type;
-			nextToken.Value = sTokenTable[i].Value;
-			bRet = true;
-			break;
-		}
-	}
-	return bRet;
-}
-long Parse(const string& value)
-{
-	long ans = 0;
-	bool neg = false;
-	Token NToken;
-	int CUV = 1;
-
-	while (GetToken(value, NToken))
-	{
-		switch (NToken.Type)
-		{
-		case TYPE_NEGATIVE:
-			neg = true;break;
-
-		case TYPE_NUMBER:
-			ans += NToken.Value * CUV;break;
-
-		case TYPE_UNIT:
-			if (NToken.Value >= CUV)
-				CUV = NToken.Value;
-			else
-				CUV *= NToken.Value;
-			break;
-		}
-		if (t == cnt)
-			break;
-	}
-	if (neg)
-		ans = -ans;
-	return ans;
-}
-int main() 
-{
-	string s;
-	while(getline(cin, s) && !s.empty())
-	{
-		t = 0;
-		Token NextToken;
-		GetToken(s, NextToken);
-		cout << Parse(s) << endl;
+		find(diction, s, dict_num);
+		cin >> s;
 	}
 	system("pause");
 	return 0;
+}
+void find(char dic[][16], char s[], int n)
+{
+	//基本性质：相同或相似单词的长度，与该单词最多差1 
+	//property：if the query is the same as the word in dictionary, or it is a replacement, then the difference of the lengths of two words will be 1 at most 
+	struct box candidate;
+	candidate.top = -1;
+	int i, j, exist, len, diff;
+	//exist记录是否找到相同单词，len记录查询单词长度，diff表示两个单词相差的字母数 
+	//\'exist\' is used to record if it is in the dictionary, \'len\' is used to record the length of the query, \'diff\' represents the amount of different characters between two words 
+	len = strlen(s);
+	exist = 0;
+	//检查词典中的每一个单词(each word in the dictionary) 
+	for (i = 0; i <= n; i++)
+	{
+		if (strlen(dic[i]) == len) //长度相等，可能相同，也可能是替换一个字母(the same length, check if they are the same or the word is the replacement of the query) 
+		{
+			if (strcmp(dic[i], s) == 0)
+			{
+				exist = 1;
+				break;
+			}
+			else
+			{
+				diff = 0;
+				for (j = 0; j < len && diff < 2; j++) //查看对应的字母不同的地方是否大于两处(//check how many different characters are there between the word and the query) 
+				{
+					if (s[j] != dic[i][j])
+						diff++;
+				}
+				if (j == len && diff < 2)
+				{
+					candidate.top++;
+					candidate.num[candidate.top] = i;
+				}
+			}
+		}
+		else
+		{
+			if (strlen(dic[i]) == len + 1)
+			{
+				diff = 0;
+				for (j = 0; j < len && diff < 2; j++)
+				{
+					if (s[j] != dic[i][j + diff]) //有时候需要跳过词典单词中的一位进行比较(skip a character of the word in the dictionary when compare two words, if is needed) 
+					{
+						diff++;
+						j--;
+					}
+				}
+				if (j == len && diff < 2) //记录相似单词(//record the replacements) 
+				{
+					candidate.top++;
+					candidate.num[candidate.top] = i;
+				}
+			}
+			else
+			{
+				if (strlen(dic[i]) == len - 1)
+				{
+					diff = 0;
+					for (j = 0; j < len - 1 && diff < 2; j++)
+					{
+						if (s[j + diff] != dic[i][j])
+							//skip a character of the word in the dictionary when compare two words, if is needed 
+						{
+							diff++;
+							j--;
+						}
+					}
+					if (j == len - 1 && diff < 2)
+					{
+						candidate.top++;
+						candidate.num[candidate.top] = i;
+					}
+				}
+			}
+		}
+	}
+	if (exist == 1)
+	{
+		cout << s << " is correct" << endl;
+	}
+	else
+	{
+		cout << s << ':';
+		for (j = 0; j <= candidate.top; j++)
+		{
+			cout << ' ' << dic[candidate.num[j]];
+		}
+		cout << endl;
+	}
 }
