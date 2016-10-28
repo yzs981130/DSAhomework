d52b18233c73c3099a0558e2116efcfd2a672c06
Tizen Ye <yzs981130@126.com>
2016-10-29 00:22:30 +0800

    二叉堆

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,58 +1,77 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <sstream>
-#include <string>
+#include <stdio.h>
+#include <algorithm>
 using namespace std;
-#define MAXN 100010
-int innerOrder[MAXN], backOrder[MAXN], d[MAXN];;
-struct BNode
+#define MAXSIZE 100000 
+struct data
 {
-	int value;
-	BNode *Left;
-	BNode *Right;
-};
-BNode *build(int _innerStart, int _innerEnd, int _backStart, int _backEnd)
-{
-	BNode *root = new BNode;
-	root->value = backOrder[_backEnd];
-	int n = 0;
-	while (innerOrder[_innerStart + n] != root->value && n < MAXN)
-		n++;
-	if (n)
-		root->Left = build(_innerStart, _innerStart + n - 1, _backStart, _backStart + n - 1);
-	else
-		root->Left = nullptr;
-	if (_innerStart + n < _innerEnd)
-		root->Right = build(_innerStart + n + 1, _innerEnd, _backStart + n, _backEnd - 1);
-	else
-		root->Right = nullptr;
-	return root;
-}
-void Travel(BNode *root)
-{
-	if (root)
+	int size;
+	int heap[1000005];
+	void swim(int x)
 	{
-		cout << root->value << ' ';
-		Travel(root->Left);
-		Travel(root->Right);
+		while (x>1)
+		{
+			if (heap[x] < heap[x >> 1])
+				swap(heap[x], heap[x >> 1]);
+			else break;
+			x >>= 1;
+		}
 	}
-}
+	void sink(int x)
+	{
+		int mn = x;
+		int l = (x << 1), r = ((x << 1) + 1);
+		if (l <= size && heap[l] < heap[mn])
+			mn = l;
+		if (r <= size && heap[r] < heap[mn])
+			mn = r;
+		if (mn != x)
+		{
+			swap(heap[mn], heap[x]);
+			sink(mn);
+		}
+	}
+	void push(int a)
+	{
+		heap[++size] = a;
+		swim(size);
+	}
+	void pop()
+	{
+		heap[1] = heap[size--];
+		sink(1);
+	}
+	void clear()
+	{
+		size = 0;
+		memset(heap, 0, sizeof(heap));
+	}
+}q;
 int main()
 {
-	int i = 0;
-	while (cin >> innerOrder[i++])
+	int n, T;
+	scanf("%d", &T);
+	while (T--)
 	{
-		if (cin.get() != ' ')   //遇到回车则停止输入  
-			break;
+		q.clear();
+		scanf("%d", &n);
+		for (int i = 1; i <= n; i++)
+		{
+			int opt, x;
+			scanf("%d", &opt);
+			if (opt == 1)
+			{
+				scanf("%d", &x);
+				q.push(x);
+			}
+			else
+			{
+				printf("%d\n", q.heap[1]);
+				q.pop();
+			}
+		}
 	}
-	i = 0;
-	while (cin >> backOrder[i++])
-	{
-		if (cin.get() != ' ')   //遇到回车则停止输入  
-			break;
-	}
-	BNode *root = build(0, i - 1, 0, i - 1);
-	Travel(root);
 	system("pause");
 	return 0;
 }
