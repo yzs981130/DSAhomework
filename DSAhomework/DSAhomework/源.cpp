0502978bb521d3a2f3a69abc2f74bf4f93fd0d5b
Tizen Ye <yzs981130@126.com>
2016-11-22 22:35:59 +0800

    数组取数

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -7,15 +7,38 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int p[1000010];
+#define OFFSET 1000000
+#define MAXN 2000100
+int a[MAXN];
 int main()
 {
-	int n, k;
-	scanf("%d %d", &n, &k);
-	k--;
+	int n, t;
+	scanf("%d %d", &n, &t);
+	int m;
 	for (int i = 0; i < n; i++)
-		scanf("%d", &p[i]);
-	nth_element(p, p + k, p + n);
-	printf("%d\n", p[k]);
+	{
+		scanf("%d", &m);
+		m += OFFSET;
+		a[m] += 1;
+	}
+	int cnt = 0;
+	for (int i = 0; i < MAXN; i++)
+	{
+		if (!a[i])continue;
+		if (i + t < 0 || i + t > MAXN)continue;
+		if (!a[i + t])continue;
+		a[i]--;
+		if (!a[i + t])
+		{
+			a[i]++;
+			continue;
+		}
+		a[i + t]--;
+		cnt++;
+		a[i]++;
+		a[i + t]++;
+	}
+	printf("%d\n", cnt);
+	system("pause");
 	return 0;
 }
