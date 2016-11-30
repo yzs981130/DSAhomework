20b0795a82bb3d5a12f19d497531cbf7c2d3fa8e
Tizen Ye <yzs981130@126.com>
2016-11-30 21:29:27 +0800

    败者树

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,72 +1,96 @@
-#include <stdio.h>
-#include<iostream> 
+#include <iostream>
+#include <cstdio>
 using namespace std;
-int m, size;
-int str[1001];
-int A[1001];
-class MinHeap { // 最小堆ADT定义
-public:
-	int * heapArray; // 存放堆数据的数组
-	int CurrentSize; // 当前堆中元素数目
-	int MaxSize; // 堆所能容纳的最大元素数目
-	void BuildHeap(); // 建堆
-	MinHeap(int* array, const int n); // 构造函数,n为最大元素数目
-	void SiftDown(int left); // 筛选法
-};
-void MinHeap::SiftDown(int position) {
-	int i = position; //标识父结点
-	int j = 2 * i + 1; //标识关键值较小的子结点
-	int  temp = heapArray[i]; //保存父结点
-	while (j<CurrentSize) { //过筛
-		if ((j<CurrentSize - 1) && (heapArray[j]>heapArray[j + 1]))
-			j++; //j指向数值较小的子结点
-		if (temp>heapArray[j]) {
-			heapArray[i] = heapArray[j];
-			i = j; j = 2 * j + 1; //向下继续
-		}
-		else break;
+
+int n, m, _index, val;
+int LowExt, offset;//最后一层外部节点数量、最后一层以上的节点数量
+int ini[1000], b[2000];//b:内部节点，保存败者的下标，ini：初始的数组
+
+int loser(int a, int b)//val大的是败者
+{
+	return ini[a]>ini[b] ? a : b;
+}
+int winner(int a, int b)
+{
+	return ini[a]>ini[b] ? b : a;
+}
+void play(int father, int left, int right)//竞赛
+{
+	int temp1 = 0, temp2 = 0;
+	b[father] = loser(left, right);
+	temp1 = winner(left, right);
+	while (father>1 && father % 2 == 1)//沿着右子树向上比较
+	{
+		temp2 = winner(temp1, b[father / 2]);
+		b[father / 2] = loser(temp1, b[father / 2]);//败者留在父节点
+		temp1 = temp2;
+		father /= 2;//再向上找父节点
 	}
-	heapArray[i] = temp;
+	b[father / 2] = temp1;//胜者放入b[0]或左孩子
 }
-void MinHeap::BuildHeap() {
-	for (int i = CurrentSize / 2 - 1; i >= 0; i--) //反复调用筛选函数
-		SiftDown(i);
-}
-MinHeap::MinHeap(int *array, const int n) {
-	CurrentSize = n;
-	MaxSize = n; //初始化堆容量为n
-	heapArray = new int[MaxSize]; //创建堆空间
-	for (int i = 0; i<n; i++) {
-		heapArray[i] = array[i];
-	}
-	BuildHeap(); //此处进行堆元素的赋值工作
-}
-void replacementSelection(int *A) {
-	//建立最小值堆
-	MinHeap H(A, size);
-	int last = size - 1;
-	int len = m<last ? m : size;
-	for (int i = 0; i<len; i++) {
-		cout << H.heapArray[0] << " ";  	//堆的最小值
-		if (str[i] >= H.heapArray[0]) 	H.heapArray[0] = str[i];
-		else {//否则用last位置记录代替根结点，把r放到last
-			H.heapArray[0] = H.heapArray[last];
-			H.heapArray[last] = str[i];
-			H.CurrentSize--;
-			last--;
-		}
-		if (last != 0)
-			H.SiftDown(0);  //堆调整 
+void replay(int i)
+{
+	int father = 0;
+	if (i <= LowExt)
+		father = (i + offset) / 2;
+	else
+		father = (i - LowExt + n - 1) / 2;
+	b[0] = winner(i, b[father]);//保存当前胜利节点
+	b[father] = loser(i, b[father]);//父节点保存败者
+	for (; (father / 2) >= 1; father /= 2)
+	{
+		int temp = 0;
+		temp = winner(b[father / 2], b[0]);//将胜者和父节点比较
+		b[father / 2] = loser(b[father / 2], b[0]);
+		b[0] = temp;
 	}
 }
-int main() {
-	cin >> m >> size;
-	int i;
-	for (i = 0; i<m; i++) {
-		cin >> str[i];
+void make(int *a)//构造初始的败者树
+{
+	int i = 0, s = 0, size = n;
+	for (s = 1; s * 2 <= n - 1; s += s);   //倒数第二层节点数
+	LowExt = 2 * (size - s);
+	//cout<<s<<endl;
+	offset = 2 * s - 1;
+	for (i = 2; i <= LowExt; i += 2)
+	{
+		play((offset + i) / 2, i - 1, i);//最底层外部节点比赛
 	}
-	for (i = 0; i<size; i++)
-		cin >> A[i];
-	replacementSelection(A);
+	if (size % 2)//外部节点数目是奇数，不能满足两两比较，要特殊处理
+	{
+		play(n / 2, b[(n - 1) / 2], LowExt + 1);//暂存在父节点中的左胜者和3号节点比赛
+		i = LowExt + 3;
+	}
+	else
+		i = LowExt + 2;
+	for (; i <= n; i += 2)//处理完前面节点，再处理剩余节点
+	{
+		play((i - LowExt + n - 1) / 2, i - 1, i);//其他的外部节点的比较
+	}
+
+}
+
+int main()
+{
+	scanf("%d%d", &n, &m);
+	for (int i = 1; i <= n; i++)
+		scanf("%d", &ini[i]);
+
+	make(b);
+
+	for (int i = 0; i<n; i++)
+		printf("%d ", ini[b[i]]);
+	printf("\n");
+
+	while (m--)
+	{
+		scanf("%d%d", &_index, &val);
+		ini[_index + 1] = val;
+		replay(_index + 1);
+		for (int i = 0; i<n; i++)
+			printf("%d ", ini[b[i]]);
+		printf("\n");
+	}
+
 	return 0;
 }
