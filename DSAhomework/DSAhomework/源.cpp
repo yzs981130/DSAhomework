f9d01f69592e1458827f6b8c407db079d53e8175
Tizen Ye <yzs981130@126.com>
2016-12-03 09:02:50 +0800

    多项式加法

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,96 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstdio>
 using namespace std;
-
-int n, m, _index, val;
-int LowExt, offset;//最后一层外部节点数量、最后一层以上的节点数量
-int ini[1000], b[2000];//b:内部节点，保存败者的下标，ini：初始的数组
-
-int loser(int a, int b)//val大的是败者
+struct item
 {
-	return ini[a]>ini[b] ? a : b;
-}
-int winner(int a, int b)
+	int k;
+	int ex;
+	item *next;
+};
+bool Equal = false;
+item *search(item *head, int exp, int k)
 {
-	return ini[a]>ini[b] ? b : a;
+	while (head->next)
+	{
+		if (head->next->ex == exp)
+		{
+			head->next->k += k;
+			Equal = true;
+		}
+		if (head->next->ex < exp)
+			break;
+		head = head->next;
+	}
+	return head;
 }
-void play(int father, int left, int right)//竞赛
-{
-	int temp1 = 0, temp2 = 0;
-	b[father] = loser(left, right);
-	temp1 = winner(left, right);
-	while (father>1 && father % 2 == 1)//沿着右子树向上比较
-	{
-		temp2 = winner(temp1, b[father / 2]);
-		b[father / 2] = loser(temp1, b[father / 2]);//败者留在父节点
-		temp1 = temp2;
-		father /= 2;//再向上找父节点
-	}
-	b[father / 2] = temp1;//胜者放入b[0]或左孩子
-}
-void replay(int i)
-{
-	int father = 0;
-	if (i <= LowExt)
-		father = (i + offset) / 2;
-	else
-		father = (i - LowExt + n - 1) / 2;
-	b[0] = winner(i, b[father]);//保存当前胜利节点
-	b[father] = loser(i, b[father]);//父节点保存败者
-	for (; (father / 2) >= 1; father /= 2)
-	{
-		int temp = 0;
-		temp = winner(b[father / 2], b[0]);//将胜者和父节点比较
-		b[father / 2] = loser(b[father / 2], b[0]);
-		b[0] = temp;
-	}
-}
-void make(int *a)//构造初始的败者树
-{
-	int i = 0, s = 0, size = n;
-	for (s = 1; s * 2 <= n - 1; s += s);   //倒数第二层节点数
-	LowExt = 2 * (size - s);
-	//cout<<s<<endl;
-	offset = 2 * s - 1;
-	for (i = 2; i <= LowExt; i += 2)
-	{
-		play((offset + i) / 2, i - 1, i);//最底层外部节点比赛
-	}
-	if (size % 2)//外部节点数目是奇数，不能满足两两比较，要特殊处理
-	{
-		play(n / 2, b[(n - 1) / 2], LowExt + 1);//暂存在父节点中的左胜者和3号节点比赛
-		i = LowExt + 3;
-	}
-	else
-		i = LowExt + 2;
-	for (; i <= n; i += 2)//处理完前面节点，再处理剩余节点
-	{
-		play((i - LowExt + n - 1) / 2, i - 1, i);//其他的外部节点的比较
-	}
-
-}
-
 int main()
 {
-	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &ini[i]);
-
-	make(b);
-
-	for (int i = 0; i<n; i++)
-		printf("%d ", ini[b[i]]);
-	printf("\n");
-
-	while (m--)
+	int n;
+	scanf("%d", &n);
+	while (n--)
 	{
-		scanf("%d%d", &_index, &val);
-		ini[_index + 1] = val;
-		replay(_index + 1);
-		for (int i = 0; i<n; i++)
-			printf("%d ", ini[b[i]]);
+		item *head = NULL, *current = NULL;
+		int k, exp;
+		for (int i = 0; i < 2; i++)
+		{
+			while (scanf("%d %d", &k, &exp) && exp >= 0)
+			{
+				current = new item;
+				current->k = k;
+				current->ex = exp;
+				current->next = NULL;
+				if (head == NULL)
+					head = current;
+				else if (exp > head->ex)
+				{
+					current->next = head;
+					head = current;
+				}
+				else if (exp == head->ex)
+				{
+					head->k += k;
+					delete current;
+				}
+				else
+				{
+					item *temp = search(head, current->ex, current->k);
+					if (!Equal)
+					{
+						current->next = temp->next;
+						temp->next = current;
+					}
+					Equal = false;
+				}
+			}
+		}
+		current = head;
+		while (current->next)
+		{
+			if (current->k != 0)
+				printf("[ %d %d ] ", current->k, current->ex);
+			current = current->next;
+		}
+		if (current->k != 0)
+			printf("[ %d %d ] ", current->k, current->ex);
 		printf("\n");
+		while (head != NULL)
+		{
+			item *temp = head->next;
+			delete head;
+			head = temp;
+		}
 	}
-
+	system("pause");
 	return 0;
 }
