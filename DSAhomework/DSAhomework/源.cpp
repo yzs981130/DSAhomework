5223e4ed09a74273766411d299e75e5e07bff0ad
Tizen Ye <yzs981130@126.com>
2016-12-05 22:02:31 +0800

    发现它，抓住它

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,30 +1,75 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<iostream>
-#include<cstdio>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <cstring>
 using namespace std;
-const int MAX = 1000000;
-vector<int> c;
-int a[MAX], b[MAX];
+#define MAXN 100010
+int p[MAXN], AB[MAXN];
+int find(int x)
+{
+	return p[x] == x ? x : (p[x] = find(p[x]));
+}
+void unite(int x, int y)
+{
+	x = find(x);
+	y = find(y);
+	if (x == y)
+		return;
+	p[x] = y;
+}
 int main()
 {
-	int n, m;
-
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &a[i]);
-
-	scanf("%d", &m);
-	for (int i = 0; i < m; i++)
-		scanf("%d", &b[i]);
-
-	std::vector<int>::iterator it;
-	c.resize(n + m);
-	it = set_symmetric_difference(a, a + n, b, b + m, c.begin());
-	c.resize(it - c.begin());
-
-	for (int i = 0; i < c.size(); i++)
-		printf("%d ", c[i]);
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		int n, m;
+		cin >> n >> m;
+		int a, b;
+		for (int i = 1; i <= n; i++) 
+		{
+			p[i] = i;
+			AB[i] = 0;
+		}
+		for (int i = 0; i < m; i++)
+		{
+			char c;
+			cin >> c;
+			if (c == 'D')
+			{
+				cin >> a >> b;
+				if (AB[a] == 0 && AB[b] == 0)
+					AB[a] = b, AB[b] = a;
+				else if (AB[a] == 0) 
+				{
+					AB[a] = b;
+					unite(a, AB[b]);
+				}
+				else if (AB[b] == 0) 
+				{
+					AB[b] = a;
+					unite(b, AB[a]);
+				}
+				else 
+				{
+					unite(a, AB[b]);
+					unite(b, AB[a]);
+				}
+			}
+			else
+			{
+				cin >> a >> b;
+				if (find(a) == find(b))
+					printf("In the same gang.\n");
+				else if (find(a) == find(AB[b]))
+					printf("In different gangs.\n");
+				else
+					printf("Not sure yet.\n");
+			}
+		}
+	}
+	system("pause");
 	return 0;
 }
