0b03bc9b9944533d3a1820e762372e2013a15556
Tizen Ye <yzs981130@126.com>
2016-12-08 23:36:46 +0800

    朱刘算法

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,70 +1,134 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <map>
-#include <string>
-#include <cstring>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<algorithm>
 using namespace std;
-#define MAXN 30
-#define INFI 99999
-bool flag[MAXN];
-struct Edge
+const int inf = 0x3f3f3f3f;
+const int maxn = 1005;
+struct point
 {
-	int a, b;
-	int weight;
-}edge[200];
-int cmp(Edge a, Edge b)
+	double x, y;
+}p[maxn];
+struct node
 {
-	return a.weight < b.weight;
+	int u, v;
+	double len;
+}edge[maxn*maxn];
+int pre[maxn], id[maxn], vis[maxn];
+double in[maxn];
+double dis(point a, point b)
+{
+	return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
+}
+double dir_mst(int root, int n, int m)
+{
+	double ans = 0;
+	while (1)
+	{
+		for (int i = 0; i<n; ++i)
+		{
+			in[i] = inf;
+		}
+		for (int i = 0; i<m; ++i)
+		{
+			int u = edge[i].u, v = edge[i].v;
+			if (edge[i].len<in[v] && u != v)
+			{
+				pre[v] = u; in[v] = edge[i].len;
+			}
+		}
+		for (int i = 0; i<n; ++i)
+		{
+			if (i == root)
+			{
+				continue;
+			}
+			if (in[i] == inf)
+			{
+				return -1;	//如果某点入度为零，必定找不到 
+			}
+		}
+		//检查这些边是否构成了环 
+		memset(id, -1, sizeof(id));
+		memset(vis, -1, sizeof(vis));
+		in[root] = 0;
+		int cnt = 0;
+		for (int i = 0; i<n; ++i)//标记环 
+		{
+			ans += in[i];
+			int v = i;
+			while (vis[v] != i&&id[v] == -1 && v != root)
+			{
+				vis[v] = i;
+				v = pre[v];
+			}
+			if (v != root&&id[v] == -1)//缩点 
+			{
+				for (int u = pre[v]; u != v; u = pre[u])
+				{
+					id[u] = cnt;
+				}
+				id[v] = cnt++;
+			}
+		}
+		if (cnt == 0)
+		{
+			break;//无环 
+		}
+		for (int i = 0; i<n; ++i)
+		{
+			if (id[i] == -1)
+			{
+				id[i] = cnt++;
+			}
+		}
+		//建立新图 
+		for (int i = 0; i<m; ++i)
+		{
+			int u = edge[i].u, v = edge[i].v;
+			edge[i].u = id[u];
+			edge[i].v = id[v];
+			if (id[u] != id[v])
+			{
+				edge[i].len -= in[v];
+			}
+		}
+		n = cnt;
+		root = id[root];
+	}
+	return ans;
 }
 int main()
 {
-	freopen("in.txt", "r", stdin);
-	freopen("out.txt", "w", stdout);
-	int n;
-	while (cin >> n && n != 0)
+	int n, m;
+	while (~scanf("%d%d", &n, &m))
 	{
-		memset(edge, 0, sizeof(edge));
-		memset(flag, 0, sizeof(flag));
-		int cnt_edge = 0;
-		for (int i = 0; i < n - 1; i++)
+		for (int i = 0; i<n; ++i)
 		{
-			char c;
-			int cnt;
-			cin >> c >> cnt;
-			for (int j = 0; j < cnt; j++)
+			scanf("%lf%lf", &p[i].x, &p[i].y);
+		}
+		for (int i = 0; i<m; ++i)
+		{
+			scanf("%d%d", &edge[i].u, &edge[i].v);
+			--edge[i].u; --edge[i].v;
+			if (edge[i].u != edge[i].v)
 			{
-				char d;
-				int len;
-				cin >> d >> len;
-				edge[cnt_edge].a = c - 'A';
-				edge[cnt_edge].b = d - 'A';
-				edge[cnt_edge].weight = len;
-				cnt_edge++;
+				edge[i].len = dis(p[edge[i].u], p[edge[i].v]);
+			}
+			else
+			{
+				edge[i].len = inf;
 			}
 		}
-		sort(edge, edge + cnt_edge, cmp);
-		int ans = 0, left = n - 1;
-		flag[0] = true;
-		while (left != 0)
+		double ans = dir_mst(0, n, m);
+		if (ans == -1)
 		{
-			for (int i = 0; i < cnt_edge; i++)
-			{
-				if (flag[edge[i].a] && flag[edge[i].b] || !flag[edge[i].a] && !flag[edge[i].b])
-					continue;
-				if (flag[edge[i].a] && !flag[edge[i].b])
-					flag[edge[i].b] = true;
-				else if (flag[edge[i].b] && !flag[edge[i].a])
-					flag[edge[i].a] = true;
-				ans += edge[i].weight;
-				left--;
-				i = -1;
-			}
+			printf("NO\n");
 		}
-		cout << ans << endl;
+		else
+		{
+			printf("%.2f\n", ans);
+		}
 	}
-	//system("pause");
 	return 0;
 }
