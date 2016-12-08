8d9c2efacb735bde4fe207f69d6978f754bbe5b8
Tizen Ye <yzs981130@126.com>
2016-12-08 16:09:43 +0800

    兔子和樱花

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -3,72 +3,81 @@
 #include <cstdio>
 #include <algorithm>
 #include <vector>
+#include <map>
+#include <string>
 #include <cstring>
 using namespace std;
-#define MAXN 100010
-int p[MAXN], AB[MAXN];
-int find(int x)
+#define MAXN 50
+#define INFI 99999
+map<string, int>m1;
+map<int, string>m2;
+struct dist
 {
-	return p[x] == x ? x : (p[x] = find(p[x]));
-}
-void unite(int x, int y)
+	int len;
+	int pre;
+}D[MAXN][MAXN];
+void output(int s, int e)
 {
-	x = find(x);
-	y = find(y);
-	if (x == y)
+	if (s == e)
+	{
+		cout << m2[s];
 		return;
-	p[x] = y;
+	}
+	output(s, D[s][e].pre);
+	cout << "->(" << D[D[s][e].pre][e].len << ")->" << m2[e];
 }
 int main()
 {
-	int t;
-	cin >> t;
-	while (t--)
+	int p, q, r, dis;
+	scanf("%d", &p);
+	string a, b;
+	for (int i = 0; i < p; i++)
 	{
-		int n, m;
-		cin >> n >> m;
-		int a, b;
-		for (int i = 1; i <= n; i++) 
+		cin >> a;
+		m1[a] = i;
+		m2[i] = a;
+	}
+	for (int i = 0; i < p; i++)
+		for (int j = 0; j < p; j++)
 		{
-			p[i] = i;
-			AB[i] = 0;
-		}
-		for (int i = 0; i < m; i++)
-		{
-			char c;
-			cin >> c;
-			if (c == 'D')
+			if (i != j)
 			{
-				cin >> a >> b;
-				if (AB[a] == 0 && AB[b] == 0)
-					AB[a] = b, AB[b] = a;
-				else if (AB[a] == 0) 
-				{
-					AB[a] = b;
-					unite(a, AB[b]);
-				}
-				else if (AB[b] == 0) 
-				{
-					AB[b] = a;
-					unite(b, AB[a]);
-				}
-				else 
-				{
-					unite(a, AB[b]);
-					unite(b, AB[a]);
-				}
+				D[i][j].len = INFI;
+				D[i][j].pre = -1;
 			}
 			else
 			{
-				cin >> a >> b;
-				if (find(a) == find(b))
-					printf("In the same gang.\n");
-				else if (find(a) == find(AB[b]))
-					printf("In different gangs.\n");
-				else
-					printf("Not sure yet.\n");
+				D[i][j].len = 0;
+				D[i][j].pre = i;
 			}
 		}
+	scanf("%d", &q);
+	for (int i = 0; i < q; i++) 
+	{
+		cin >> a >> b >> dis;
+		if (D[m1[a]][m1[b]].len > dis)
+		{
+			D[m1[a]][m1[b]].len = dis;
+			D[m1[a]][m1[b]].pre = m1[a];
+			D[m1[b]][m1[a]].len = dis;
+			D[m1[b]][m1[a]].pre = m1[b];
+		}
+	}
+	for(int k = 0; k < p; k++)
+		for(int i = 0; i < p; i++)
+			for (int j = 0; j < p; j++)
+				if (D[i][j].len > D[i][k].len + D[k][j].len)
+				{
+					D[i][j].len = D[i][k].len + D[k][j].len;
+					D[i][j].pre = D[k][j].pre;
+				}
+	
+	scanf("%d", &r);
+	for (int i = 0; i < r; i++)
+	{
+		cin >> a >> b;
+		output(m1[a], m1[b]);
+		cout << endl;
 	}
 	system("pause");
 	return 0;
