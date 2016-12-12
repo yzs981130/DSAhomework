0fa33926fbf347cdfb8ea38c28ac8ebdd84371c3
Tizen Ye <yzs981130@126.com>
2016-12-12 08:58:30 +0800

    等价表达式

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,63 +1,107 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstdio>
 #include <string>
-#include <cstring>
+#include <map>
 #include <stack>
-#include <algorithm>
 using namespace std;
-using namespace std;
-stack<char>s;
-int main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	string str;
-	cin >> str;
+
+map<char, int> operatorPriority;
+
+string Transform(const string& str) {
 	int len = str.length();
-	bool ans = true;
-	for (int i = 0; i < len && ans; i++)
-	{
-		char c = str[i];
-		if (c == '(' || c == '[' || c == '{')
-			s.push(c);
-		else if (c == ')')
-		{
-			if (s.empty())
+	char rt[90];
+	int cnt = 0;
+	stack<char> oper;
+	for (int i = 0; i < len; ++i) {
+		if (str[i] == '-')
+			i = i;
+		if (str[i] == ' ')
+			continue;
+		else if (isalnum(str[i]))
+			rt[cnt++] = str[i];
+		else { /* is an operator */
+			switch (str[i])
 			{
-				ans = false;
+			case'(':
+				oper.push('(');
+				break;
+			case')':
+				while (oper.top() != '(') {
+					rt[cnt++] = oper.top();
+					oper.pop();
+				}
+				oper.pop();
+				break;
+				/* + - * operator */
+			default:
+				if (!oper.empty()) {
+					char top = oper.top();
+					if (operatorPriority[top] >= operatorPriority[str[i]]) {
+						rt[cnt++] = top;
+						oper.pop();
+					}
+				}
+				oper.push(str[i]);
 				break;
 			}
-			char t = s.top();
-			if (t == '(')
-				s.pop();
-		}
-		else if (c == ']')
-		{
-			if (s.empty())
-			{
-				ans = false;
-				break;
-			}
-			char t = s.top();
-			if (t == '[')
-				s.pop();
-		}
-		else if (c == '}')
-		{
-			if (s.empty())
-			{
-				ans = false;
-				break;
-			}
-			char t = s.top();
-			if (t == '{')
-				s.pop();
 		}
 	}
-	if (s.empty() && ans)
-		cout << "Yes" << endl;
-	else
-		cout << "No" << endl;
+	while (!oper.empty()) {
+		rt[cnt++] = oper.top();
+		oper.pop();
+	}
+	rt[cnt] = '\0';
+	return string(rt);
+}
+
+
+int Calculate(string r) {
+	int len = r.length(), a, b;
+	stack<int> cal;
+	for (int i = 0; i < len; ++i) {
+		if (isalnum(r[i])) {
+			if (isdigit(r[i]))
+				cal.push(r[i] - '0');
+			else
+				cal.push(r[i]);
+		}
+		else {
+			a = cal.top();
+			cal.pop();
+			b = cal.top();
+			cal.pop();
+			switch (r[i]) {
+			case '+':
+				cal.push(b + a);
+				break;
+			case '-':
+				cal.push(b - a);
+				break;
+			case '*':
+				cal.push(b * a);
+			}
+		}
+	}
+	return cal.top();
+}
+
+int main() {
+
+	operatorPriority['+'] = 1;
+	operatorPriority['-'] = 1;
+	operatorPriority['*'] = 2;
+	operatorPriority['('] = 0;
+	int cases;
+	cin >> cases;
+	cin.get();
+	while (cases--) {
+		string exp1, exp2;
+		getline(cin, exp1);
+		getline(cin, exp2);
+		if (Calculate(Transform(exp1)) == Calculate(Transform(exp2)))
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
 	return 0;
 }
