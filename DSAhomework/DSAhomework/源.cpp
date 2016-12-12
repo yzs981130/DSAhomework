96f0fd0e3836bd76ca09be3198d7ea35aad1b3ad
Tizen Ye <yzs981130@126.com>
2016-12-12 17:35:47 +0800

    POJ 1442 Black Box

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -3,56 +3,111 @@
 #include <algorithm>
 #include <cstring>
 #include <string>
+#include <functional>
+#include <queue>
 #include <cstdio>
 #define MAXN 50010
-#define INF 99999999
 using namespace std;
-struct node
+int a[MAXN];
+priority_queue<int, vector<int> >big;
+priority_queue<int, vector<int>, greater<int> >small;
+int cnt = 0;
+struct Node
 {
-	int w, fa, lch, rch;
-	char s[111];
-	bool operator <(const node& a)const
+	int v, w;
+	int s;
+	Node *son[2];
+	void Resize()
 	{
-		return strcmp(s, a.s) <= 0;
+		s = 1;
+		if (son[0])
+			s += son[0]->s;
+		if (son[1])
+			s += son[1]->s;
 	}
-}p[MAXN];
-int n;
-void insert(int i)
+}nodes[MAXN];
+void Rotate(Node *&p, int d)
 {
-	int j = i - 1;
-	while (p[j].w < p[i].w) 
-		j = p[j].fa;
-	p[i].lch = p[j].rch;
-	p[j].rch = i;
-	p[i].fa = j;
+	Node *u = p->son[d];
+	p->son[d] = u->son[d ^ 1];
+	u->son[d ^ 1] = p;
+	p->Resize();
+	u->Resize();
+	p = u;
 }
-void solve(int rt)
+void Insert(Node *&p, int v)
 {
-	if (rt == 0) 
-		return;
-	printf("(");
-	solve(p[rt].lch);
-	printf("%s/%d", p[rt].s, p[rt].w);
-	solve(p[rt].rch);
-	printf(")");
+	if (p == NULL)
+	{
+		nodes[cnt].s = 1;
+		nodes[cnt].son[0] = NULL;
+		nodes[cnt].son[1] = NULL;
+		nodes[cnt].v = v;
+		nodes[cnt].w = rand();
+		p = &nodes[cnt];
+		cnt++;
+	}
+	else
+	{
+		int d = (v > p->v) ? 1 : 0;
+		Insert(p->son[d], v);
+		if (p->son[d]->w > p->w)
+			Rotate(p, d);
+	}
+	p->Resize();
+}
+void Remove(Node *&p, int v)
+{
+	if (p->v == v)
+	{
+		if (p->son[0] && p->son[1])
+		{
+			int d = (p->son[0]->w < p->son[1]->w) ? 1 : 0;
+			Rotate(p, d);
+			Remove(p->son[d ^ 1], v);
+		}
+		else
+		{
+			Node *u = p;
+			if (p->son[0] == NULL)
+				p = p->son[1];
+			else
+				p = p->son[0];
+			delete u;
+		}
+	}
+	else
+		Remove(p->son[v > p->v], v);
+	if (p)
+		p->Resize();
+}
+int Find(Node *p, int v)
+{
+	int ls = (p->son[0]) ? p->son[0]->s : 0;
+	if (v == ls + 1)
+		return p->v;
+	else if (v < ls + 1)
+		return Find(p->son[0], v);
+	else
+		return Find(p->son[1], v - ls - 1);
 }
 int main()
 {
-	char s[111], tmp[111];
-	while (scanf("%d", &n) != EOF && n)
+	int n, m;
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	Node *root = NULL;
+	int j = 0;
+	for (int i = 0; i < m; i++)
 	{
-		for (int i = 1; i <= n; i++)
-		{
-			scanf(" %[a-z]/%d", p[i].s, &p[i].w);
-			p[i].lch = p[i].rch = 0;
-			p[i].fa = 0;
-		}
-		sort(p + 1, p + n + 1);
-		p[0].w = INF;
-		p[0].lch = p[0].rch = p[0].fa = 0;
-		for (int i = 1; i <= n; i++) insert(i);
-		solve(p[0].rch);
-		printf("\n");
+		int t;
+		scanf("%d", &t);
+		for (; j < t; j++)
+			Insert(root, a[j]);
+		int ans = Find(root, i + 1);
+		printf("%d\n", ans);
 	}
+	system("pause");
 	return 0;
 }
