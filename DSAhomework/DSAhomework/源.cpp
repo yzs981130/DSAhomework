543af2ff3d05f043cd2a59260763510bdf0ffbab
Tizen Ye <yzs981130@126.com>
2016-12-12 09:00:18 +0800

    欧拉回路

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,54 +1,56 @@
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<vector>
-#include<queue>
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <queue>
+#include <string>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <functional>
 using namespace std;
-int n, m;
-struct node {
-	int y, di, fee;
-	node(int yy, int dii, int fe)
-	{
-		y = yy, di = dii, fee = fe;
-	}node() {}
-	bool operator < (const struct node a)const
-	{
-		if (a.di == di) return a.fee<fee;
-		return a.di<di;
-	}
-}now, nex;
-int maxx;
-int dis[110];
-vector<node> lin[110];
-priority_queue<node> q;
-int dj()
+#define MAXN 1010
+int d[MAXN];
+int p[MAXN];
+int find(int x)
 {
-	q.push(node(1, 0, 0));
-	while (!q.empty())
+	while (x != p[x])
 	{
-		now = q.top();
-
-		q.pop();
-		if (now.y == n) return now.di;
-		for (int i = 0; i<lin[now.y].size(); i++)
-		{
-
-			if (now.fee + lin[now.y][i].fee <= maxx)
-				q.push(node(lin[now.y][i].y, now.di + lin[now.y][i].di, now.fee + lin[now.y][i].fee));
-		}
+		p[x] = p[p[x]];
+		x = p[x];
 	}
-	return -1;
+	return x;
 }
-
+void unite(int x, int y)
+{
+	int fx = find(x);
+	int fy = find(y);
+	if (fx != fy)
+		p[fx] = fy;
+}
 int main()
 {
-	scanf("%d%d%d", &maxx, &n, &m);
-	for (int i = 1; i <= m; i++)
+	int n, m, a, b;
+	while (cin >> n && n != 0)
 	{
-		int aa, bb, cc, dd;
-		scanf("%d%d%d%d", &aa, &bb, &cc, &dd);
-		lin[aa].push_back(node(bb, cc, dd));
+		memset(d, 0, sizeof(d));
+		for (int i = 0; i < n; i++)
+			p[i] = i;
+		cin >> m;
+		for (int i = 0; i < m; i++)
+		{
+			cin >> a >> b;
+			unite(a, b);
+			d[a]++;
+			d[b]++;
+		}
+		bool ans = true;
+		for (int i = 0; i < n; i++)
+			if (d[i] % 2 || find(i) != find(0))
+			{
+				ans = false;
+				break;
+			}
+
+		cout << ans << endl;
 	}
-	printf("%d\n", dj());
 	return 0;
 }
