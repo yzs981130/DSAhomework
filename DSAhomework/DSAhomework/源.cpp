d26a611a10e53e7e1e2d8578314b2b17022e05a7
Tizen Ye <yzs981130@126.com>
2016-12-17 20:29:49 +0800

    POJ1274

--- DSAhomework/DSAhomework/源.cpp
+++ DSAhomework/DSAhomework/源.cpp
@@ -1,88 +1,55 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdlib.h>  
-#include <stdio.h>  
-#include <string.h>  
-#include <iostream>  
-#define MAX 1200  
+#include<iostream>
+#include<cstdio>
+#include<cstring>
 using namespace std;
-bool map[MAX][MAX], used[MAX];
-int mat[MAX];
-int p[MAX][MAX];
-int dir[8] = { 1,0,-1,0,0,1,0,-1 };
-void BFS(int n, int m)
+#define maxn 205
+int graph[maxn][maxn];
+int match[maxn];
+int book[maxn];
+int n, m, k;
+bool dfs(int u) 
 {
-	int i, k, q;
-	for (i = 1; i <= n; i++)
-		for (k = 1; k <= m; k++)
+	for (int i = 1; i <= n; i++) 
+	{
+		if (book[i] == 0 && graph[i][u] == 1) 
 		{
-			if (p[i][k] == -1)
-				continue;
-			for (q = 0; q < 8; q += 2)
+			book[i] = 1;
+			if (match[i] == 0 || dfs(match[i])) 
 			{
-				int a = i + dir[q];
-				int b = k + dir[q + 1];
-				if (p[a][b] != -1 && a >= 1 && b <= m && a <= n && b >= 1)
-					map[p[i][k]][p[a][b]] = true;
-			}
-		}
-}
-int Augment(int x, int n)
-{
-	int i;
-	for (i = 1; i <= n; i++)
-		if (!used[i] && map[x][i])
-		{
-			used[i] = true;
-			if (!mat[i] || Augment(mat[i], n))
-			{
-				mat[i] = x;
+				match[i] = u;
 				return 1;
 			}
 		}
+	}
 	return 0;
 }
-int Hungary(int n, int m)
+int main() 
 {
-	int i, sum = 0;
-	memset(mat, 0, sizeof(mat));
-	for (i = 1; i <= n; i++)
+	int cnt = 0;
+	while (scanf("%d %d", &m, &n) != EOF) 
 	{
-		memset(used, false, sizeof(used));
-		if (Augment(i, m))
-			sum++;
-	}
-	return sum;
-}
-int main()
-{
-	int n, m, k, i, x, y, tmp;
-	while (~scanf("%d%d%d", &n, &m, &k))
-	{
-		memset(p, 0, sizeof(p));
-		memset(map, false, sizeof(map));
-		tmp = k;
-		for (i = 1; i <= k; i++)
+		memset(graph, 0, sizeof(graph));
+		memset(match, 0, sizeof(match));
+		
+		int s, k;
+		for (int i = 1; i <= m; i++) 
 		{
-			scanf("%d%d", &x, &y);
-			p[y][x] = -1;
+			scanf("%d", &s);
+			for (int j = 0; j < s; j++)
+			{
+				scanf("%d", &k);
+				graph[i][k] = 1;
+			}
 		}
-		if (n*m % 2 != k % 2)
+		int sum = 0;
+		for (int i = 1; i <= m; i++) 
 		{
-			printf("NO\n");
-			continue;
+			memset(book, 0, sizeof(book));
+			if (dfs(i))
+				sum++;
 		}
-
-		int ind = 1;
-		for (i = 1; i <= n; i++)
-			for (k = 1; k <= m; k++)
-				if (p[i][k] == 0)
-					p[i][k] = ind++;
-		BFS(n, m);
-		int ans = Hungary(ind - 1, ind - 1);
-		if (ans / 2 == (n*m - tmp) / 2)
-			printf("YES\n");
-		else
-			printf("NO\n");
+		printf("%d\n", sum);
 	}
 	return 0;
 }
